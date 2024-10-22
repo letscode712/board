30db9a0da9ed20ab9bc3ba53930fb5a964063539
letscode712 <kimyoungchae0712@gamil.com>
2024-10-22 16:28:06 +0900

    10/22 second commit

--- board/react-board/webpack.config.js
+++ board/react-board/webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 
 module.exports = {
-    // other configuration
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
